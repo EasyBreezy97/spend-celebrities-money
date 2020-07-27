df1abbc56ebfe4c5a29abc34c07113147b17651a
Guram <gurikakhasia@gmail.com>
2020-07-27 19:10:58 +0400

    Deploy on heroku

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
 
 app.use(express.json()); //body parser
 
@@ -8,7 +10,18 @@ const PORT = 5000 || process.env.PORT;
 
 const routes = require('./routes')
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    console.log(__dirname)
+    // Set static folder
+    app.use(express.static('client/build'));
 
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
+console.log(__dirname)
 app.use('/api',routes)
 
 app.listen(PORT, () => {
